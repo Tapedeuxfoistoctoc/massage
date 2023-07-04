4bff37e1d61565aa89b6583a621b4df82f156116
Tapedeuxfoistoctoc <piikme7@gmail.com>
2023-07-04 09:24:05 +0400

    sitemassage

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Massage/css.css">
+    
+</head>        
+
+<body>
+
+
+
+    <header class="main">
+
+
+
+        <nav>
+
+            <div class="logo">
+
+                <img src="../Massage/massage_logo_header-1.png" alt="Logo">
+
+            </div>
+
+            <ul id="decal">
+
+                <li><a href="#">HOMEPAGE</a></li>
+                <li><a href="">ABOUT US</a></li>
+                <li><a href="">PAGES & PROJECTS</a></li>
+                <li><a href="">BLOG</a></li>
+                <li><a href="">CONTACT</a></li>
+
+
+            </ul>
+
+
+
+
+
+
+
+        </nav>
+
+        <h1>WELCOME
+            IN BEAUTY PARADISE
+            In publishing and graphic design, lorem ipsum is common to cry </h1>
+
+    </header>
+
+</html>
