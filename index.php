cd94fc28cd933f95aa8cc52cc948554f0f26453c
Tapedeuxfoistoctoc <piikme7@gmail.com>
2023-07-07 09:44:44 +0400

    mise a jour du site massage

--- index.php
+++ index.php
@@ -1,57 +1,408 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Massage</title>
 
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../Massage/css.css">
-    
-</head>        
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="css.css">
+</head>
 
 <body>
+    <header id="header_lul">
 
+        <nav id="menu">
+            <nav>
+                <img src="../massage/image/massage_logo_header-1.png" alt="" />
+                <div class="liennav">
+                    <div class="dropdown">
+                        <button class="dropbtn">HOMEPAGE &#11167;</button>
+                        <div class="dropdown-content">
+                            <a href="#">HOMEPAGE 1</a>
+                            <a href="#">HOMEPAGE 2</a>
+                            <a href="#">HOMEPAGE 3</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">ABOUT US &#11167;</button>
+                        <div class="dropdown-content">
+                            <a href="#">ABOUT US 1</a>
+                            <a href="#">ABOUT US 2</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">PAGES & PROJECT &#11167;</button>
+                        <div class="dropdown-content">
+                            <a href="#">OUR TEAM</a>
+                            <a href="#">GALLERY</a>
+                            <a href="#">SINGLE SERVICE WITH SIDEBARS</a>
+                            <a href="#">PORTEFOLIO MODULE</a>
+                            <a href="#">LIST OF BLURBS</a>
+                            <a href="#">LIST OF ICONES</a>
+                            <a href="#">404 PAGE</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">BLOG &#11167;</button>
+                        <div class="dropdown-content">
+                            <a href="#">BLOG PAGES</a>
+                            <a href="#">BLOG POST WITH RIGHT SLIDEBAR</a>
+                            <a href="#">BLOG POST WITH RIGHT SLIDEBAR</a>
+                            <a href="#">BLOG POST WITH >LEFT SLIDEBAR</a>
+                            <a href="#">BLOG POST FULLWIDTH</a>
+                        </div>
+                    </div>
+                    <input type="button" value="CONTACT" />
+                </div>
+            </nav>
+            <li class="lienmenu"><a class="lien_lul" href="#"><img id="logo" src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/04/massage_logo_header-1.png"></a></li>
+            <li class="lienmenu"><a class="lien_lul" href="#"> HOMEPAGE  </a></li>
+            <li class="lienmenu"><a class="lien_lul" href="#">ABOUT US   </a></li>
+            <li class="lienmenu"><a class="lien_lul" href="#"> PAGES & PROJECTS  </a></li>
+            <li class="lienmenu"><a class="lien_lul" href="#"> BLOG  </a></li>
+            <li class="lienmenu"> <button id="bouton_lul"> CONTACT </button>  </li> 
+        </nav>
+        <div id="headbar"></div>
+    </header>
 
+    <main>
+        <section id="sect1">
+            <div class="section">
+                <img src="./img/icon05.png" alt="">
+                <h1>WELCOME</h1><br>
+                <h1>IN BEAUTY PARADISE</h1>
+                <h3>In publishing and graphic design, lorem ipsum is common to cry</h3><br>
+                <li class="lienmenu"> <button id="bouton_lule"> BOOK A MASSAGE </button> </li>
+            </div>
 
-    <header class="main">
+        </section>
 
-
-
-        <nav>
-
-            <div class="logo">
-
-                <img src="../Massage/massage_logo_header-1.png" alt="Logo">
+        <section id="sect2">
+            <div class="section1">
+                <h3>what we do</h3>
+                <h1>OUR SERVICES</h1>
+                <br>
+                <p>In publishing and graphic design, lorem ipsum is common placeholder text <br> used to demonstrate the
+                    graphic elements of a document or </p>
 
             </div>
 
-            <ul id="decal">
+            
+            <div class="container">
 
-                <li><a href="#">HOMEPAGE</a></li>
-                <li><a href="">ABOUT US</a></li>
-                <li><a href="">PAGES & PROJECTS</a></li>
-                <li><a href="">BLOG</a></li>
-                <li><a href="">CONTACT</a></li>
+                <div class="contaner">
+                    <div class="box">
+                        <div class="imager">
+                            <a href="#"></a><img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage12.jpg" alt=""> 
+                        </div>
+                        <div class="content">
+                            <h3>Proffessional Massage</h3>
+                            <p>In publishing and graphic design, lorem ipsum is common placeholder text used to
+                                demonstrate the graphic elements of a document or visual presentation, such as web
+                                pages, typography, and graphical layout. It is a form of “greeking”.</p>
+                        </div>
+                    </div>
+                </div>
 
 
+                <div class="boite2 ">
+
+                    
+                    <div class="contaner ">
+                        <div class=" imgCardTwo">
+
+                            <div class="imager">
+                                <a href="#"></a><img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage33.jpg" alt="">
+                            </div>
+
+                            <div class="content">
+                                <h3>Proffessional Manicure</h3>
+                                <p>In publishing and graphic design, lorem ipsum is common placeholder text used to
+                                    demonstrate the graphic elements of a document or visual presentation, such as web
+                                    pages, typography, and graphical layout. It is a form of “greeking”.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="contaner">
+                    <div class="imgCardTree">
+                        <div class="imager">
+                            <a href="#"></a><img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage32.jpg" alt=""> 
+                        </div>
+                        
+                        <div class="content">
+                            <h3>Make-Up Artist</h3>
+                            <p>In publishing and graphic design, lorem ipsum is common placeholder text used to
+                                demonstrate the graphic elements of a document or visual presentation, such as web
+                                pages, typography, and graphical layout. It is a form of “greeking”.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+
+           
+            <div>
+                <p>what makes us unique</p>
+                <h2>Why To Choose Our Salon</h2>
+                <div>
+                    <ul>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur
+                                quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui
+                                perspiciatis! Impedit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur
+                                quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui
+                                perspiciatis! Impedit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur
+                                quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui
+                                perspiciatis! Impedit.</p>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur
+                                quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui
+                                perspiciatis! Impedit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur
+                                quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui
+                                perspiciatis! Impedit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ipsam consectetur, fugit, dolorem tenetur
+                                quia adipisci doloremque reprehenderit, unde ut porro rerum molestias atque quidem eos ducimus qui
+                                perspiciatis! Impedit.</p>
+                        </li>
+                    </ul>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage18.png" alt="">
+                </div>
+            </div>
+
+            
+            <div class="bar2">
+                <div class="barr2">
+                    <h2><strong>Introducing Massage Studio</strong></h2>
+                    <button class="dropbtn">BOOK A MASSAGE TODAY &#11167;></button>
+                </div>
+            </div>
+
+            
+            <div>
+                <div>
+                    <h2></h2>
+                    <hr>
+                    <p></p>
+                </div>
+                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1.jpg" alt="">
+            </div>
+
+            
+            <div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon08.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon01.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon03.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon04.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/icon10.png" alt="">
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </div>
+
+            
+            <div>
+                <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage39.jpg" alt="">
+                <div>
+                    <h2></h2>
+                    <hr>
+                    <p></p>
+                </div>
+            </div>
+
+            
+            <div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage21-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage20-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+                <div>
+                    <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage07-400x250.jpg" alt="">
+                    <h3></h3>
+                    <p></p>
+                    <button></button>
+                </div>
+            </div>
+
+            
+            <div>
+
+                
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage18.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage37-1.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage35-1-1.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://massage-theme.richardpruzek.com/wp-content/uploads/2020/02/massage38.png" alt="">
+                    </div>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+
+
+    <footer>
+
+        <div id="fin_page">
+
+            <ul>
+                <li>
+                    <h3>MASSAGE TYPES</h3>
+                </li>
+                <li><a href="">Sport Massage</a></li>
+                <li><a href="">Baby Massage</a></li>
+                <li><a href="">Thai Massage</a></li>
+                <li><a href="">Classic Massage</a></li>
+                <li><a href="">Physiotherapy</a></li>
+                <li><a href="">Physiotherapy</a></li>
             </ul>
 
+            <ul>
+                <li>
+                    <h3>ABOUT US</h3>
+                </li>
+                <li><a href="">Our Masseurs</a></li>
+                <li><a href=""> Our Clients</a></li>
+                <li><a href="">Our Values</a></li>
+                <li><a href="">Photogallery</a></li>
+                <li><a href="">Our Philosophy</a></li>
+                <li><a href="">Our 2020 Goals</a></li>
+            </ul>
+
+            <ul>
+                <li>
+                    <h3>CONTACT US</h3>
+                </li>
+                <li>
+                    <p>+49 8568 523 525</p>
+                    <p>+43 8568 856 235</p>
+                </li>
+                <li>
+                    <p>info@massage.com</p>
+                    <p>hello@myspa.de</p>
+                </li>
+                <li>
+                    <p>info@massage.com</p>
+                    <p>helo@myspa.de</p>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <h3>SUBSCRIBE</h3>
+                </li>
+                <li><input type="text" placeholder="NAME"></li>
+                <li><input type="email" placeholder="EMAIL"></li>
+                <li><input type="submit" placeholder="SUBSCRIBE"></li>
+            </ul>
+
+        </div>
+        <div id="barre_blanc"></div>
+        <div>
+        </div>
+
+    </footer>
 
 
+</body>
+
+</html>
 
 
-
-
-        </nav>
-
-        <h1>WELCOME
-            IN BEAUTY PARADISE
-            In publishing and graphic design, lorem ipsum is common to cry </h1>
-
-    </header>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+</script>
 
 </html>
